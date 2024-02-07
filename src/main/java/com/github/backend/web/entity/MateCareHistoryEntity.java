533b27de4b51f712fcab8f2eccbdd7f98d22a628
linxizhu1209 <146171215+linxizhu1209@users.noreply.github.com>
2024-02-07 15:42:14 +0900

    feat: mateCareHistory엔티티 생성
    - 메이트의 도움 내역을 관리하는 엔티티를 생성
    - 도움 내역을 상태로 구분하기 위해 상태 관련 enum(MateCareStatus) 추가

---
+++ src/main/java/com/github/backend/web/entity/MateCareHistoryEntity.java
@@ -0,0 +1,37 @@
+package com.github.backend.web.entity;
+
+
+import com.github.backend.web.entity.enums.CareStatus;
+import com.github.backend.web.entity.enums.MateCareStatus;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Builder
+@AllArgsConstructor
+@Table(name = "mate_care_history_table")
+public class MateCareHistoryEntity {
+    @Id
+    @Column(name="mate_care_history_cid")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Schema(description = "메이트 도움 내역 고유 아이디")
+    private Long mateCareHistoryCid;
+
+    @Column
+    @Schema(description = "메이트의 도움 상태",example = "취소")
+    private MateCareStatus mateCareStatus;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "mate_cid", referencedColumnName = "mate_cid")
+    private MateEntity mate;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "care_cid", referencedColumnName = "mate_cid")
+    private CareEntity care;
+
+
+}
