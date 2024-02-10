f0627c6f87173480f9c5367c1599a3000ae6974a
linxizhu1209 <146171215+linxizhu1209@users.noreply.github.com>
2024-02-10 13:20:51 +0900

    fix: master&mate 관련 dto swagger설정 추가
    - master/mate 관련된 dto에 swagger에 보여질 예시를 추가

--- src/main/java/com/github/backend/web/dto/mates/CaringDetailsDto.java
+++ src/main/java/com/github/backend/web/dto/mates/CaringDetailsDto.java
@@ -1,6 +1,7 @@
 package com.github.backend.web.dto.mates;
 
 import com.github.backend.web.entity.enums.Gender;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -13,13 +14,23 @@ import java.time.LocalTime;
 @Setter
 @Builder
 public class CaringDetailsDto {
+
+@Schema(description = "동행 신청 고유 번호",example = "1")
 private Long careCid;
+@Schema(description = "사용자 아이디",example = "linxizhu129")
 private String userId;
+@Schema(description = "동행 날짜",example = "2024.2.10")
 private LocalDate date;
+@Schema(description = "동행 시작 시간",example = "09:00")
 private LocalTime startTime;
+@Schema(description = "동행 종료 시간",example = "11:00")
 private LocalTime finishTime;
+@Schema(description = "목적지",example = "00병원")
 private String arrivalLoc;
+@Schema(description = "비용",example = "50,000")
 private Long cost;
+@Schema(description = "도움서비스 내용",example = "서울 OO병원 동행 서비스")
 private String content;
+@Schema(description = "희망성별",example = "MEN/WOMEN")
 private Gender gender;
 }
