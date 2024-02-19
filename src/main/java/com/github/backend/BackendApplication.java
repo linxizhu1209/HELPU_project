59c2fbb6c2089cd0d676aa3096fd46498c00dcf4
JungWanSEO <sjwan0326@gmail.com>
2024-02-19 21:05:38 +0900

    fix: swagger 다시 수정
    - swagger https 적용

--- src/main/java/com/github/backend/BackendApplication.java
+++ src/main/java/com/github/backend/BackendApplication.java
@@ -1,11 +1,14 @@
 package com.github.backend;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@OpenAPIDefinition(servers = {@Server(url = "https://helpu-service.site", description = "Default Server URL")})
 public class BackendApplication {
 
 	public static void main(String[] args) {
